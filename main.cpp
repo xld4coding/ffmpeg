a4e675d71595fdf4b10b2e5d7b9930c165615418
Chenweijie <wjchen@hnu.edu.cn>
2020-07-08 09:59:45 +0800

    commit .vscode

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main(){
-    cout<<"Hello world!"<<endl;
+    vector<int>a={1,2,3,4,5,6,7,8};
+    for(auto num:a){
+        cout<<num
+            <<" ";
+    }
+    cout<<endl;
     return 0;
 }
